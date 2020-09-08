28cf3f6d2399955d7e0609392835c7075f222b83
alireza <alirezamika@gmail.com>
2020-09-08 13:40:44 +0430

    add ability to remove unwanted scraping rules

--- autoscraper/utils.py
+++ autoscraper/utils.py
@@ -1,6 +1,14 @@
+import random
+import string
+
+
 def unique(item_list):
     unique_list = []
     for item in item_list:
         if item not in unique_list:
             unique_list.append(item)
     return unique_list
+
+def get_random_str(n):
+    chars = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for i in range(n))
