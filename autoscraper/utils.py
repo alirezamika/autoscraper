6dff734c3ea4cc9b9ec3fb9b3f22989702bff479
Ilya Shuklin <ilya.shuklin@gmail.com>
2020-09-10 13:08:30 +0300

    unique refactor

--- autoscraper/utils.py
+++ autoscraper/utils.py
@@ -3,11 +3,7 @@ import string
 
 
 def unique(item_list):
-    unique_list = []
-    for item in item_list:
-        if item not in unique_list:
-            unique_list.append(item)
-    return unique_list
+    return list(set(item_list))
 
 def get_random_str(n):
     chars = string.ascii_lowercase + string.digits
