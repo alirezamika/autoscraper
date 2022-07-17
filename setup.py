26bc6bf78a14753f5dca6856999d96d5374dc64d
Alireza <alireza@msa.local>
2022-07-17 21:48:27 +0430

    update version to 1.1.14

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="autoscraper",
-    version="1.1.12",
+    version="1.1.14",
     description="A Smart, Automatic, Fast and Lightweight Web Scraper for Python",
     long_description_content_type="text/markdown",
     long_description=long_description,
